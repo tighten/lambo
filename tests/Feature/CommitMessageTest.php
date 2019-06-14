e408e040a1db621c8feb175df4a49d62b75123c7
Andrew Morgan <andrewmile@gmail.com>
2019-06-14 13:15:37 -0500

    Rename test

--- tests/Feature/CommitMessageTest.php
+++ tests/Feature/CommitMessageTest.php
@@ -5,7 +5,7 @@ namespace App\Feature;
 use App\Facades\OptionManager;
 use Tests\TestCase;
 
-class GitMessageTest extends TestCase
+class CommitMessageTest extends TestCase
 {
     /** @test */
     function setting_an_initial_commit_message_at_runtime()
