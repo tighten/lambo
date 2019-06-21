20f9ba5c6ace6327d0d5936c84f39256e89f2029
Andrew Morgan <andrewmile@gmail.com>
2019-06-21 16:37:08 -0500

    Update commit message config location

--- app/ActionsOnInstall/InitializeGit.php
+++ app/ActionsOnInstall/InitializeGit.php
@@ -15,12 +15,12 @@ class InitializeGit extends BaseAction
     {
         $showOutput = false;
 
-        $gitMessage = config('lambo.config.gitMessage');
+        $commitMessage = config('lambo.config.commit');
         $directory = config('lambo.store.project_path');
 
         $this->shell->inDirectory($directory, 'git init', $showOutput);
         $this->shell->inDirectory($directory, 'git add .', $showOutput);
-        $this->shell->inDirectory($directory, "git commit -m \"{$gitMessage}\"", $showOutput);
+        $this->shell->inDirectory($directory, "git commit -m \"{$commitMessage}\"", $showOutput);
 
         $this->console->info('Git repository initialized.');
     }
