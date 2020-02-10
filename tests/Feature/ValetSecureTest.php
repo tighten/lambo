8f174a9108930e4586136d7bcfea97251d908154
Jon Sugar <jonathansugar@mac.com>
2020-02-10 17:00:51 +0000

    Test App\Actions\ValetSecure.

---
+++ tests/Feature/ValetSecureTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\ValetSecure;
+use App\Shell\Shell;
+use Exception;
+use Illuminate\Support\Facades\Config;
+use Tests\Feature\Fakes\FakeProcess;
+use Tests\TestCase;
+
+class ValetSecureTest extends TestCase
+{
+    /** @test */
+    function it_runs_valet_link()
+    {
+        $this->fakeLamboConsole();
+        $shell = $this->mock(Shell::class);
+
+        Config::set('lambo.store.valet_secure', true);
+
+        $shell->shouldReceive('execInProject')
+            ->with('valet secure')
+            ->once()
+            ->andReturn(FakeProcess::success());
+
+        app(ValetSecure::class)();
+    }
+
+    /** @test */
+    function it_throws_an_exception_if_the_after_script_fails()
+    {
+        $this->fakeLamboConsole();
+        $shell = $this->mock(Shell::class);
+
+        Config::set('lambo.store.valet_secure', true);
+
+        $command = 'valet secure';
+        $shell->shouldReceive('execInProject')
+            ->with($command)
+            ->once()
+            ->andReturn(FakeProcess::fail($command));
+
+        $this->expectException(Exception::class);
+
+        app(ValetSecure::class)();
+    }
+}
