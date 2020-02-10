384dde491522d82c56d927a6604e29b7bb1eb5ed
Jon Sugar <jonathansugar@mac.com>
2020-02-10 16:43:30 +0000

    Test App\Actions\RunAfterScript.

---
+++ tests/Feature/RunAfterScriptTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Actions\RunAfterScript;
+use App\Shell\Shell;
+use Exception;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
+use Tests\Feature\Fakes\FakeProcess;
+use Tests\TestCase;
+
+class RunAfterScriptTest extends TestCase
+{
+    /** @test */
+    function it_runs_the_after_script_if_one_exists()
+    {
+        $this->fakeLamboConsole();
+        $shell = $this->mock(Shell::class);
+
+        Config::set('home_dir', '/my/home/dir');
+        $afterScriptPath = Config::get('home_dir') . '/.lambo/after';
+
+        File::shouldReceive('exists')
+            ->with($afterScriptPath)
+            ->andReturn(true)
+            ->globally()
+            ->ordered();
+
+        $shell->shouldReceive('execInProject')
+            ->with("sh " . $afterScriptPath)
+            ->once()
+            ->andReturn(FakeProcess::success())
+            ->globally()
+            ->ordered();
+
+        app(RunAfterScript::class)();
+    }
+
+    /** @test */
+    function it_throws_an_exception_if_the_after_script_fails()
+    {
+        $this->fakeLamboConsole();
+        $shell = $this->mock(Shell::class);
+
+        Config::set('home_dir', '/my/home/dir');
+        $afterScriptPath = Config::get('home_dir') . '/.lambo/after';
+
+        File::shouldReceive('exists')
+            ->with($afterScriptPath)
+            ->andReturn(true)
+            ->globally()
+            ->ordered();
+
+        $command = "sh " . $afterScriptPath;
+        $shell->shouldReceive('execInProject')
+            ->with($command)
+            ->once()
+            ->andReturn(FakeProcess::fail($command))
+            ->globally()
+            ->ordered();
+
+        $this->expectException(Exception::class);
+
+        app(RunAfterScript::class)();
+    }
+}
