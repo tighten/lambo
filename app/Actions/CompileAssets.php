7e8aee93eaa4b5b45fe592ba9b31d813558ac165
Jon Sugar <jonathansugar@mac.com>
2020-02-10 15:59:19 +0000

    Test App\Actions\CompileAssets.

--- app/Actions/CompileAssets.php
+++ app/Actions/CompileAssets.php
@@ -3,18 +3,18 @@
 namespace App\Actions;
 
 use App\Shell\Shell;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\File;
 
 class CompileAssets
 {
     use LamboAction;
 
     protected $shell;
+    protected $silentDevScript;
 
-    public function __construct(Shell $shell)
+    public function __construct(Shell $shell, SilentDevScript $silentDevScript)
     {
         $this->shell = $shell;
+        $this->silentDevScript = $silentDevScript;
     }
 
     public function __invoke()
@@ -23,43 +23,20 @@ class CompileAssets
             return;
         }
 
-        $this->addSilentDevScript();
+        $this->silentDevScript ->add();
 
         $this->logStep('Compiling project assets');
 
-        $process = $this->shell->execInProject("npm run dev {$this->extraOptions()}");
+        $process = $this->shell->execInProject("npm run dev{$this->extraOptions()}");
 
         $this->abortIf(! $process->isSuccessful(), 'Compilation of project assets did not complete successfully', $process);
 
-        $this->removeSilentDevScript();
+        $this->silentDevScript->remove();
 
         $this->info('Project assets compiled successfully.');
     }
-
     public function extraOptions()
     {
-        return config('lambo.store.show-output') ? '' : '--silent';
-    }
-
-    public function addSilentDevScript()
-    {
-        $originalPackageJson = config('lambo.store.project_path') . '/package.json';
-        File::copy($originalPackageJson, config('lambo.store.project_path') . '/package-original.json');
-        File::replace($originalPackageJson, $this->silentPackageJson($originalPackageJson));
-    }
-
-    public function silentPackageJson($originalPackageJsonPath)
-    {
-        $packageJson = json_decode(File::get($originalPackageJsonPath), true);
-        $silentDevelopmentCommand = str_replace('--progress', '--no-progress', Arr::get($packageJson, 'scripts.development'));
-        Arr::set($packageJson, 'scripts.development', $silentDevelopmentCommand);
-        return json_encode($packageJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-    }
-
-    public function removeSilentDevScript()
-    {
-        $packageJson = config('lambo.store.project_path') . '/package.json';
-        $originalPackageJson = config('lambo.store.project_path') . '/package-original.json';
-        File::move($originalPackageJson, $packageJson);
+        return config('lambo.store.with_output') ? '' : ' --silent';
     }
 }
