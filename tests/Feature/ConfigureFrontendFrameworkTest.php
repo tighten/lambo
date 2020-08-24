88477370ee3e2c42e2d6365190191a7d48038e4a
Jon Sugar <jonathansugar@mac.com>
2020-08-24 15:57:18 +0100

    test: specifically check that each frontend framework is installed.

--- tests/Feature/ConfigureFrontendFrameworkTest.php
+++ tests/Feature/ConfigureFrontendFrameworkTest.php
@@ -6,7 +6,6 @@ use App\Actions\ConfigureFrontendFramework;
 use App\Actions\LaravelUi;
 use App\LamboException;
 use App\Shell;
-use Illuminate\Support\Facades\Config;
 use Tests\Feature\Fakes\FakeProcess;
 use Tests\TestCase;
 
@@ -23,32 +22,33 @@ class ConfigureFrontendFrameworkTest extends TestCase
     }
 
     /** @test */
-    function it_installs_the_specified_front_end_framework()
+    function it_installs_vue()
     {
-        Config::set('lambo.store.frontend', 'foo-frontend');
-
-        $this->laravelUi->shouldReceive('install')
-            ->once()
-            ->globally()
-            ->ordered();
-
-        $this->shell->shouldReceive('execInProject')
-            ->with('php artisan ui foo-frontend --quiet')
-            ->once()
-            ->andReturn(FakeProcess::success())
-            ->globally()
-            ->ordered();
-
-        app(ConfigureFrontendFramework::class)(['foo-frontend', 'bar-frontend']);
+        $this->shouldInstallFrontendFramework('vue');
+        app(ConfigureFrontendFramework::class)();
     }
 
     /** @test */
-    function it_does_not_install_a_frontend_framework_when_none_is_specified()
+    function it_installs_react()
+    {
+        $this->shouldInstallFrontendFramework('react');
+        app(ConfigureFrontendFramework::class)();
+    }
+
+    /** @test */
+    function it_installs_bootstrap()
+    {
+        $this->shouldInstallFrontendFramework('bootstrap');
+        app(ConfigureFrontendFramework::class)();
+    }
+
+    /** @test */
+    function it_skips_frontend_framework_installation()
     {
         $shell = $this->spy(Shell::class);
         $laravelUi = $this->spy(LaravelUi::class);
 
-        $this->assertEmpty(Config::get('lambo.store.frontend'));
+        $this->assertEmpty(config('lambo.store.frontend'));
 
         app(ConfigureFrontendFramework::class);
 
@@ -57,49 +57,52 @@ class ConfigureFrontendFrameworkTest extends TestCase
     }
 
     /** @test */
-    function it_throws_and_exception_if_the_ui_framework_installation_fails()
+    function it_throws_a_lambo_exception_if_ui_framework_installation_fails()
     {
-        Config::set('lambo.store.frontend', 'foo-frontend');
-
-        $this->laravelUi->shouldReceive('install')
-            ->once()
-            ->globally()
-            ->ordered();
-
-        $command = 'php artisan ui foo-frontend --quiet';
-        $this->shell->shouldReceive('execInProject')
-            ->with($command)
-            ->once()
-            ->andReturn(FakeProcess::fail($command))
-            ->globally()
-            ->ordered();
+        $this->shouldFailFrontendFrameworkInstallation('vue');
 
         $this->expectException(LamboException::class);
 
-        app(ConfigureFrontendFramework::class)(['foo-frontend', 'bar-frontend']);
+        app(ConfigureFrontendFramework::class)();
     }
 
     /** @test */
-    function it_removes_the_quiet_flag_when_show_output_is_enabled()
+    function it_installs_the_framework_with_verbose_output()
     {
-        Config::set('lambo.store.frontend', 'foo-frontend');
-        Config::set('lambo.store.with_output', true);
+        config(['lambo.store.frontend' => 'vue']);
+        config(['lambo.store.with_output' => true]);
+
+        $this->shouldInstallFrontendFramework('vue', true, false);
+
+        app(ConfigureFrontendFramework::class)();
+    }
+
+    private function shouldFailFrontendFrameworkInstallation(string $frontendFramework)
+    {
+        $this->shouldInstallFrontendFramework($frontendFramework, false);
+    }
+
+    private function shouldInstallFrontendFramework(string $frontendFramework, bool $success = true, bool $quiet = true): void
+    {
+        config(['lambo.store.frontend' => $frontendFramework]);
+
+        $command = sprintf("php artisan ui %s%s", $frontendFramework, $quiet ? ' --quiet' : '');
 
         $this->laravelUi->shouldReceive('install')
             ->once()
             ->globally()
             ->ordered();
 
-        $command = 'php artisan ui foo-frontend';
-        $this->shell->shouldReceive('execInProject')
+        $expectation = $this->shell->shouldReceive('execInProject')
             ->with($command)
             ->once()
-            ->andReturn(FakeProcess::fail($command))
             ->globally()
             ->ordered();
 
-        $this->expectException(LamboException::class);
-
-        app(ConfigureFrontendFramework::class)(['foo-frontend', 'bar-frontend']);
+        if ($success) {
+            $expectation->andReturn(FakeProcess::success());
+        } else {
+            $expectation->andReturn(FakeProcess::fail($command));
+        }
     }
 }
