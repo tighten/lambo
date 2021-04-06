25315c7206dab944f5cf888cb8443d8bdf4623e2
Jon <jonathansugar@mac.com>
2021-04-06 18:27:46 +0100

    Remove extra lines

--- app/Actions/DisplayHelpScreen.php
+++ app/Actions/DisplayHelpScreen.php
@@ -18,26 +18,22 @@ class DisplayHelpScreen
     public function __invoke()
     {
         app('console-writer')->newLine();
-
         app('console-writer')->text('<comment>Commands (lambo COMMANDNAME):</comment>');
         foreach ($this->commands as $command => $description) {
             $spaces = $this->makeSpaces(strlen($command));
             app('console-writer')->text("  <info>{$command}</info>{$spaces}{$description}");
         }
-
         app('console-writer')->newLine();
         app('console-writer')->text('<comment>Usage:</comment>');
         app('console-writer')->text('  lambo help');
         app('console-writer')->text('  lambo [common options] edit-config [--editor=<editor>]');
         app('console-writer')->text('  lambo [common options] edit-after [--editor=<editor>]');
         app('console-writer')->text('  lambo [common options] new myApplication [--editor=<editor>] [options]');
-
         app('console-writer')->newLine();
         app('console-writer')->text('<comment>Options (lambo new myApplication):</comment>');
         foreach ((new Options())->all() as $option) {
             app('console-writer')->text($this->createCliStringForOption($option));
         }
-
         app('console-writer')->newLine();
         app('console-writer')->text('<comment>Common options:</comment>');
         foreach ((new Options())->common() as $option) {
